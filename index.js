16ab31514bc29f9394e1172973ce46fad3364215
Emmanuel Ezenwigbo <emmanuelezenwigbo@gmail.com>
2020-05-13 21:22:09 +0100

    Created Amines Translation Function

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
+
+// Match RNA strings to their actual meaning
+let codons = ( () => {
+  let AUG,UUU,UUC,UUA,UUG,UCU,UCC,UCA,UCG,UAU,UAC,UGU,UGC,UGG,UAA,UAG,UGA;
+  AUG = "Methionine";
+  UUU = UUC = "Phenylalanine";
+  UUA = UUG = "Leucine";
+  UCU = UCC = UCA = UCG = "Serine";
+  UAU = UAC = "Tyrosine";
+  UGU = UGC = "Cysteine";
+  UGG = "Tryptophan";
+  UAA = UAG = UGA = "STOP";
+  return {AUG,UUU,UUC,UUA,UUG,UCU,UCC,UCA,UCG,UAU,UAC,UGU,UGC,UGG,UAA,UAG,UGA}
+})();
+
+
+// Translate Function
 const translate = (string="") => {
-    const proteins=[];
-    let obtainedproteins=[];
+ 
+  //Create array to hold protein names 
+  const proteins=[];
+
+  // divide strings into 3 with this for loop
+  for (let index = 0; index < string.length; index+=3) {
+    const protein = codons[string.substring(index,index+3)];
     
-    let codons = {AUG:"Methionine",
-      UUU:"Phenylalanine", UUC:"Phenylalanine",
-      UUA:"Leucine", UUG:"Leucine",
-      UCU:"Serine", UCC:"Serine", UCA:"Serine", UCG:"Serine",
-      UAU:"Tyrosine", UAC:"Tyrosine",
-      UGU:"Cysteine", UGC:"Cysteine",
-      UGG:"Tryptophan",
-      UAA:"STOP", UAG:"STOP", UGA:"STOP"
-    };
-  
-   //type code here
-    return obtainedproteins;
-  
-  };
+    // throw error if protein is undefined
+    if (protein === undefined)
+      throw new Error ('Invalid codon')
+
+    if (protein === 'STOP')
+      break;
+
+    //Push proteins to protein arrays 
+    proteins.push(protein);
+  }
+
+  return proteins
+};
+
+translate('UCUUCCUCAUCG')
 
   module.exports = translate;
